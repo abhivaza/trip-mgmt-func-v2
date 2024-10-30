aec33777a65e2ee94748f1b048c5be2df88b4137
Abhishek Vaza <abhishekvaza@gmail.com>
2024-10-30 12:53:25 -0400

    Update index.ts

--- trip-mgmt-func/src/index.ts
+++ trip-mgmt-func/src/index.ts
@@ -9,19 +9,19 @@ import { vertexAI } from "@genkit-ai/vertexai";
 
 functions.onInit(() => {
   // initialize firebase
-  // admin.initializeApp({
-  //   credential: admin.credential.cert(
-  //     getFirebaseConfig().serviceAccount as admin.ServiceAccount
-  //   ),
-  // });
+  admin.initializeApp({
+    credential: admin.credential.cert(
+      getFirebaseConfig().serviceAccount as admin.ServiceAccount
+    ),
+  });
 
   // Uncomment for running local DB
-  admin.initializeApp();
-  const firestore = admin.firestore();
-  firestore.settings({
-    host: "localhost:8080", // Default emulator port
-    ssl: false,
-  });
+  // admin.initializeApp();
+  // const firestore = admin.firestore();
+  // firestore.settings({
+  //   host: "localhost:8080", // Default emulator port
+  //   ssl: false,
+  // });
 
   // initialize genkit
   configureGenkit({
@@ -44,4 +44,4 @@ functions.onInit(() => {
   });
 });
 
-export const api = functions.https.onRequest(app);
+export const api = functions.runWith({ memory: "512MB" }).https.onRequest(app);
